9c0044864ea897f44fd46dd5a6d140df925f581a
galangam <galang1659@gmail.com>
2025-10-14 08:23:13 +0700

    Menambahkan file 02 untuk program menulis kode php di dalam html

---
+++ sintaks-dasar-php/02-menulis-kode-php-di-html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo "Belajar PHP" ?></title>
+    </head>
+    <body>
+        <?php
+            echo "<h1>saya sedang belajar PHP</h1><br>";
+            echo "<p>Belajar PHP hingga jadi master di <b>Petani kode   </b></p>";
+        ?>
+    </body>
+</html>
