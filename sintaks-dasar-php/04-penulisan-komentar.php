ccd1664dfa0c9823a715446b32481a904ff41d2a
galangam <galang1659@gmail.com>
2025-10-14 08:58:33 +0700

    Menambahkan file 04 untuk program penulisan komentar di PHP

---
+++ sintaks-dasar-php/04-penulisan-komentar.php
@@ -0,0 +1,13 @@
+<?php 
+
+// ini adalah komentar
+
+
+/* ini adalah komentar yang
+lebih dari satu baris */
+echo "<h3>Komentar di PHP dapat ditulis dengan dua cara:<h3>
+<br>
+Menggunakan tanda // untuk komentar satu baris <br>
+Menggunakan tanda /* untuk komentar lebih dari satu baris.";
+
+?>
