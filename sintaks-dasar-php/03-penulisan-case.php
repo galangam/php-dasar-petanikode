b520a32360e0d65ff48a6acab5161368f2f03348
galangam <galang1659@gmail.com>
2025-10-14 08:48:21 +0700

    Menambahkan file 03 untuk program penulisan case PHP

---
+++ sintaks-dasar-php/03-penulisan-case.php
@@ -0,0 +1,17 @@
+<?php
+
+$nama = "Galang AM";
+$umur = "16";
+$kelas = "xi rpl";
+$sekolah = "SMKN 2 SUBANG";
+
+echo "nama saya adalah $nama<br>";
+echo "saya berumur $umur tahun<br>";
+echo "saya sekarang berada di dalam kelas $kelas <br>";
+echo "saya sekarang sekolah di $sekolah <br>";
+
+echo "<h3>PHP adalah bahasa pemrograman yang bersifat case sensitive. Artinya, huruf besar (kapital) dan huruf kecil akan dibedakan.</h3>";
+echo "<h3>Untuk menghindari ini, kita harus konsisten dalam penamaan variabel dan konstanta.
+
+Gunakan nama variabel dengan huruf kecil saja <br> dan konstanta dengan huruf kapital.</h3>";
+?>
