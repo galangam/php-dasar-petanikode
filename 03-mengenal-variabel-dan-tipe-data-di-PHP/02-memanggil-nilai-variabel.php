87f67ea56bb97584fd775b0475ec37fa6f94c6d9
galangam <galang1659@gmail.com>
2025-10-17 09:59:55 +0700

    Menambahkan file 02 untuk program mengambil nilai variabel di PHP

---
+++ 03-mengenal-variabel-dan-tipe-data-di-PHP/02-memanggil-nilai-variabel.php
@@ -0,0 +1,11 @@
+<?php
+// Deklarasi variabel
+$nama = "Galang";
+$umur = 16;
+$hobi = "Ngoding";
+
+// Menampilkan nilai variabel
+echo "Nama saya adalah " . $nama . "<br>";
+echo "Umur saya " . $umur . " tahun<br>";
+echo "Hobi saya adalah " . $hobi;
+?>
