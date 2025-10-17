8afd48c4c063d12ff0e789c42e04051772d0028a
galangam <galang1659@gmail.com>
2025-10-17 14:40:14 +0700

    Menambahkan file 06 untuk program operator bitwise di PHP

---
+++ 04-operator-di-php/06-operator-bitwise.php
@@ -0,0 +1,29 @@
+<?php
+
+$a = 60;
+$b = 13;
+
+// bitwise AND
+$c = $a & $b;
+echo "$a & $b = $c";
+echo "<hr>";
+
+// bitwise OR
+$c = $a | $b;
+echo "$a | $b = $c";
+echo "<hr>";
+
+// bitwise XOR
+$c = $a ^ $b;
+echo "$a ^ $b = $c";
+echo "<hr>";
+
+// Shift Left
+$c = $a << $b;
+echo "$a << $b = $c";
+echo "<hr>";
+
+// Shift Right
+$c = $a >> $b;
+echo "$a >> $b = $c";
+echo "<hr>";
