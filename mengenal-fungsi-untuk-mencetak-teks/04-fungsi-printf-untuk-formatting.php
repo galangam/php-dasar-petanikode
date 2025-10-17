2e2ebe0c697650ed75c3e91128619f83095c60ec
galangam <galang1659@gmail.com>
2025-10-17 09:18:30 +0700

    Menambahkan file 04 untuk program fungsi printf untuk formatting teks

---
+++ mengenal-fungsi-untuk-mencetak-teks/04-fungsi-printf-untuk-formatting.php
@@ -0,0 +1,14 @@
+<?php
+   $txt = "petanikode.com";
+printf("Aku belajar pemrograman PHP di %s<br>", $txt);
+
+// misalkan kita punya bilangan dengan notasi E seperti ini
+$harga = 100000;
+
+// jika kita cetak dengan echo:
+echo "Harganya adalah Rp $harga<br>";
+
+// jika kita cetak dengan printf
+printf("Harganya adalah Rp %.2f <br >", $harga);
+
+echo 'hari ini hari jum\'at';
