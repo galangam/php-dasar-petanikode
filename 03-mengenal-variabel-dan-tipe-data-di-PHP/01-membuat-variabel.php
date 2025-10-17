8b2794324408b88dd829a21c61fe996577b62436
galangam <galang1659@gmail.com>
2025-10-17 09:59:17 +0700

    Menambahkan file 01 untuk program membuat variabel di PHP

---
+++ 03-mengenal-variabel-dan-tipe-data-di-PHP/01-membuat-variabel.php
@@ -0,0 +1,19 @@
+<?php
+
+// ini adalah contoh variabel
+$nama_barang = "Kopi C++";
+$harga = 4000;
+
+// membuat variabel baru
+$stok = 40;
+
+// mengisi ulang variabel dengan nilai baru
+$stok = 34;
+
+$nama_barang = "Buku PHP";   // <-- menggunakan camelCase
+$namaPembeli = "Petani Kode"; 
+
+// tiga variabel ini akan dianggap berbeda karena bersifat case sensitive
+$Belajar = "";
+$BELAJAR = "";
+$belajar = "";
