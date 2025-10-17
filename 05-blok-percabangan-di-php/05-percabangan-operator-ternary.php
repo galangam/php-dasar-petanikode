e16281390a208bed67696efbd100559d54f9fdb8
galangam <galang1659@gmail.com>
2025-10-17 19:44:45 +0700

    Menambahkan file 05 untuk program percabangan operator ternary di PHP

---
+++ 05-blok-percabangan-di-php/05-percabangan-operator-ternary.php
@@ -0,0 +1,9 @@
+<?php
+$umur = 18; // variabel umur
+
+// menggunakan operator ternary
+$pesan = ($umur >= 17) ? "Kamu sudah cukup umur untuk membuat KTP." : "Kamu belum cukup umur untuk membuat KTP.";
+
+// menampilkan hasil
+echo $pesan;
+?>
