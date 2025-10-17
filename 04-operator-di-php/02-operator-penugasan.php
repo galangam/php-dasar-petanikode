009911d71aec61cdddf255654f6e1d02153b22c0
galangam <galang1659@gmail.com>
2025-10-17 14:36:32 +0700

    Menambahkan file 02 untuk program operator penugasan di PHP

---
+++ 04-operator-di-php/02-operator-penugasan.php
@@ -0,0 +1,15 @@
+<?php
+
+$umur= 16;
+
+// ini operator aritmatika
+$umur = $umur + 5;
+
+// maka nilai umur akan sama dengan 16 + 5 = 21
+
+// ini operator penugasan
+$umur -= 10;
+
+// sekarang nilai umur akan menjadi 21 - 10 = 11
+
+echo "$umur";
