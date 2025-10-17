aa8693de8935cf93a61c45532c267741f076295d
galangam <galang1659@gmail.com>
2025-10-17 09:16:50 +0700

    Menambahkan file 03 untuk program menggabungkan string atau teks di PHP

---
+++ mengenal-fungsi-untuk-mencetak-teks/03-menggabung-string-teks.php
@@ -0,0 +1,10 @@
+   <?php
+$txt1 = "Menggabungkan String atau Teks di PHP";
+$txt2 = "petanikode.com";
+$x = 106;
+$y = 2;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Aku belajar pemrograman PHP di " . $txt2 . "<br>";
+echo $x / $y;
+?>    
