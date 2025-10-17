feb7c11c68b34452c9d69729c1de6a71ad05ce12
galangam <galang1659@gmail.com>
2025-10-17 14:37:31 +0700

    Menambahkan file 03 untuk program operator increment dan decrement di PHP

---
+++ 04-operator-di-php/03-operator-increment-dan-decrement.php
@@ -0,0 +1,18 @@
+<?php
+
+$score = 0;
+
+// operator increment menggunakan simbol ++ untuk menambah +1 (tambah satu)
+$score++;      // 0 + 1 = 1
+$score++;      // 1 + 1 = 2
+$score++;     // 2 + 1 = 3
+
+// sedangkan decrement menggunakan simbol -- untuk mengurangi -1 (kurangi dengan satu).
+$score--;       // 3 - 1 = 2
+$score--;       // 2 - 1 = 1
+$score--;       // 1 - 1 = 0
+$score--;       // 0 - 1 = -1 
+$score--;       // -1 - 1 = -2 
+
+// untuk menampilkan hasil akhir score yaitu -2
+echo $score;
