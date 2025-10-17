4babca300068d4b89c107ed037127208aa1f0957
galangam <galang1659@gmail.com>
2025-10-17 14:41:05 +0700

    Menambahkan file 07 untuk program operator ternary di PHP

---
+++ 04-operator-di-php/07-operator-ternary.php
@@ -0,0 +1,19 @@
+<?php
+
+$suka = true;
+
+// menggunakan operator ternary
+$jawab = $suka ? "iya": "tidak" ; 
+
+// menampilkan jawaban
+echo $jawab;
+
+echo "<br>";
+
+$malamhari = false;
+
+// menggunakan operator ternary
+$jawab = $malamhari ? "iya": "tidak";
+
+// menampilkan jawaban
+echo $jawab;
