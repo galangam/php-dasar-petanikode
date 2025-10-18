c9a79988b01cab2aea9ed495af93836f39ac59a5
galangam <galang1659@gmail.com>
2025-10-18 10:35:19 +0700

    Menambahkan file 01 untuk program membuat array di PHP

---
+++ 07-struktur-data-array-di-php/01-membuat-array.php
@@ -0,0 +1,14 @@
+<?php
+
+// membuat array kosong
+$buah = array();
+$hobi = [];
+
+// membuat array sekaligus mengisinya
+$minuman = array("Kopi", "Teh", "Jus Jeruk");
+$makanan = ["Nasi Goreng", "Soto", "Bubur"];
+
+// membuat array dengan mengisi indeks tertentu
+$anggota[1] = "Dian";
+$anggota[2] = "Muhar";
+$anggota[0] = "Petani Kode";
